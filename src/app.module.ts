2202104cb4efe29e94c32ca802a17f2a13d27c6a
Tawhid IB <tawhidib@Tawhids-MacBook-Pro.local>
2024-04-08 11:18:23 +0600

    fix: database connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { appENVValidationSchema } from './app-env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: appENVValidationSchema,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI'),
+        auth: {
+          username: config.get<string>('MONGO_USER'),
+          password: config.get<string>('MONGO_PASS'),
+        },
+        dbName: config.get<string>('MONGO_DB_NAME'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({ forbidNonWhitelisted: true, whitelist: true }),
+  );
   await app.listen(3000);
 }
 bootstrap();
